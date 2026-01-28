08d4dab3f8e3fcf3be1979ff8885689a747861e5
AryanPuch <puchimad@sheridancollege.ca>
2026-01-28 17:55:16 -0500

    Initial commit.

---
+++ src/main/java/ice4/projectaryan/Greeting.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+package ice4.projectaryan;
+
+/**
+ *
+ * @author Aryan Puchimada, January 2026
+ */
+public class Greeting {
+
+}
---
+++ src/main/java/ice4/projectaryan/ProjectAryan.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package ice4.projectaryan;
+
+/**
+ *
+ * @author Aryan Puchimada, January 2026
+ */
+public class ProjectAryan {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
